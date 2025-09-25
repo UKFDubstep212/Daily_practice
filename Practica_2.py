f71091b3bf25abea7a71cf90a595f30ffa05f4e3
UKFDubstep212 <122425489+UKFDubstep212@users.noreply.github.com>
2025-09-24 19:19:50 -0600

    Progression of my python's practice

--- Practica_2.py
+++ Practica_2.py
@@ -6,27 +6,32 @@ import numpy as np
 # B = rng.standard_normal((n,n))
 
 rng = np.random.default_rng(42)
-A = rng.integers(low=0, high=10, size=(3,3)) #Matriz 3x3 de numeros enteros
-print(A)
+# A = rng.integers(low=0, high=10, size=(3,3)) #Matriz 3x3 de numeros enteros
+# print(A)
 
-B = rng.random((3,3))#Matiz aleatoria 3x3 de numeros entre 0 y 1
-print(B)
+# B = rng.random((3,3))#Matiz aleatoria 3x3 de numeros entre 0 y 1
+# print(B)
 
-C = rng.uniform(-3,3,(3,3)) #Matriz aleatoria 3x3 entre -3 y 3
-print(C)
+# C = rng.uniform(-3,3,(3,3)) #Matriz aleatoria 3x3 entre -3 y 3
+# print(C)
 
-Z = rng.standard_normal((4,4)) #Distribucion Gaussiana
-X = 100 + 15*Z
-print(X)
+# Z = rng.standard_normal((4,4)) #Distribucion Gaussiana
+# X = 100 + 15*Z
+# print(X)
 
-M = rng.binomial(n=1, p=0.3, size=(3,3)) #Distribucion binomial o Bernoulli con probabilidad de 1 de 0.3
-print(M)
-print()
+# M = rng.binomial(n=1, p=0.3, size=(3,3)) #Distribucion binomial o Bernoulli con probabilidad de 1 de 0.3
+# print(M)
+# print()
 
-valores = np.array([10,20,30,40,50])
-S = rng.choice(valores, size=(3,3), replace=True)
-print(S)
-print()
-P = rng.permutation(9).reshape(3,3) #Permuta
-print(P)
+# valores = np.array([10,20,30,40,50])
+# S = rng.choice(valores, size=(3,3), replace=True) #Escoge valores aleatorios de una vector dado para establecer una matriz n x n
+# print(S)
+# print()
+# P = rng.permutation(9).reshape(3,3) #Permuta
+# print(P)
+
+ids = np.arange(100)
+
+V = rng.choice(ids, size=10, replace=False) #Para el size se puede hacer con vector o plano
+print(V)
 
---
+++ practica_3.py
@@ -0,0 +1,62 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+rng = np.random.default_rng(123)
+
+# ids = np.arange(10)
+
+# p = np.array([0.2, 0.15, 0.15, 0.1, 0.1, 0.1, 0.05, 0.05, 0.05, 0.05])
+
+# V = rng.choice(ids, size=5, replace=True, p=p)
+# print("Muestreo con pesos: ", V)
+
+# M = rng.integers(1, 10, size=(6,4)) #Matriz de numeros aleatorios enteros
+# print(M)
+
+# print(M.sum(axis=1)) #Suma las filas de la matriz creada
+# print(M.sum(axis=0)) #Suma las columans de la matriz
+# print(M.max(axis=1)) #Valor maximo de cada fila
+# print(M.min(axis=0)) #Valor minimo de cada columna
+# print(M.argmax(axis=0)) #Indice del valor maximo por columna
+
+ids = np.arange(10) #Crear valores para ids
+pesos = np.array([0.18, 0.16, 0.14, 0.12, 0.10, 0.08, 0.07, 0.06, 0.05, 0.04]) #darle peso a cada id
+
+#Declaramos nuestros parametros
+dias, numVenDia, nProd = 30, 100, 10
+
+#creamos una matriz de 0 donde guardar la ventas
+V = np.zeros((dias, nProd), dtype=int)
+
+#Simulamos la cantidad n de dias
+for d in range(dias):
+    #Le asignamos 100 productos vendidos a cada dia respecto a un peso especificado
+    d_venta = rng.choice(ids, size=numVenDia, replace=True, p=pesos)
+    #Contamos cuantos productos con ids iguales se vendieron en ese dia 
+    #para guardarlo en la matriz de 0 que cremos anteriormente
+    V[d] = np.bincount(d_venta, minlength=nProd)
+
+print(V.shape)
+
+#Vamos a analizar los datos
+ventaProducto = V.sum(axis=0)
+ventaDia = V.sum(axis=1)
+
+print("La suma total por producto es la siguiente: ", ventaProducto)
+print("La suma total por dia fue: ", ventaDia)
+print("El producto mas vendido fue: ", (np.argmax(ventaProducto)+1))
+print("El dia con mayor ventas fue: ", (np.argmax(ventaDia)+1))
+
+#Mapa de calor para ver la comparativa de cantidad de productos vendidos
+plt.imshow(V, cmap="hot", aspect="auto")
+plt.title("Venta de 30 dias")
+plt.xlabel("Producto")
+plt.ylabel("Dia")
+plt.show()
+
+plt.bar(np.arange(len(ventaProducto)), ventaProducto)
+plt.title("Ventas por producto en 30 dias")
+plt.xlabel("Producto")
+plt.ylabel("Cantidad de productos vendidos")
+plt.show()
+
